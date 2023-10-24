dd96fc7813ca9886e83eb58fd809f0520776d8ca
IshanThilanka <136796679+IshanThilanka@users.noreply.github.com>
2023-10-24 21:53:24 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,72 @@
+var fgImage = null;
+var bgImage = null;
+var fgCanvas;
+var bgCanvas;
+var outcan;
+
+function loadForegroundImage() {
+  var file = document.getElementById("fgfile");
+  fgImage = new SimpleImage(file);
+  fgCanvas = document.getElementById("fgcan");
+  fgImage.drawTo(fgCanvas);
+}
+
+function loadBackgroundImage() {
+  var file = document.getElementById("bgfile");
+  bgImage = new SimpleImage(file);
+  bgCanvas = document.getElementById("bgcan");
+  bgImage.drawTo(bgCanvas);
+  
+}
+
+function createComposite() {
+  // this function creates a new image with the dimensions of the foreground image and returns the composite green screen image
+  var output = new SimpleImage(fgImage.getWidth(),fgImage.getHeight());
+  var greenThreshold = 180;
+  for (var pixel of fgImage.values()) {
+    var x = pixel.getX();
+    var y = pixel.getY();
+    if (pixel.getGreen() > greenThreshold) {
+      //pixel is green, use background
+      var bgPixel = bgImage.getPixel(x,y);
+      output.setPixel(x,y,bgPixel);
+    }
+    else {
+      //pixel is not green, use foreground
+      output.setPixel(x,y,pixel);
+    }
+  }
+  return output;
+}
+
+function doGreenScreen() {
+  //check that images are loaded
+  if (fgImage == null  || ! fgImage.complete()) {
+    alert("Foreground image not loaded");
+  }
+  if (bgImage == null || ! bgImage.complete()) {
+    alert("Background image not loaded");
+  }
+   if (bgImage.getWidth() < fgImage.getWidth() || bgImage.getHeight() < fgImage.getHeight()) {
+    alert("Background image must be the same size or smaller than the foreground image");
+    return; // Exit the function
+  }
+
+  // clear canvases
+  //clearCanvas();
+  // call createComposite, which does green screen algorithm and returns a composite image
+  outcan = document.getElementById("outcan");
+  var finalImage = createComposite();
+  finalImage.drawTo(outcan);
+}
+
+function clearCanvas() {
+  doClear(fgCanvas);
+  doClear(bgCanvas);
+  doClear(outcan);
+}
+
+function doClear(canvas) {
+  var context = canvas.getContext("2d");
+  context.clearRect(0,0,canvas.width,canvas.height);
+}
